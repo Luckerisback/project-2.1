9d7e314364db613d319435af81650b40d89cb5d9
Luckerisback <61817870+Luckerisback@users.noreply.github.com>
2021-10-15 20:25:27 +0700

    AddSomeShit

---
+++ Project_2.1/Assets/MainMenu/Scripts/MainMenuController.cs
@@ -0,0 +1,16 @@
+﻿
+using UnityEngine;
+using UnityEngine.SceneManagement;
+
+public class MainMenuController : MonoBehaviour
+{
+   public void StartGame(int sceneNumber)
+   {
+      SceneManager.LoadScene(sceneNumber);
+   }
+
+   public void ExitGame()
+   {
+      Application.Quit();
+   }
+}
