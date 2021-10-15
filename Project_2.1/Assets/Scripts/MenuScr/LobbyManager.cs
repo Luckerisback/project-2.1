135af41b418a48bb2c7b84e4ebff797c3840347e
Frranky <srfranky228@gmail.com>
2021-10-15 19:24:52 +0700

    Project2.1-001-Lobby Manager
    Created obj Lobby manager and scr LobbyManager

--- Project_2.1/Assets/Scripts/MenuScr/LobbyManager.cs
+++ Project_2.1/Assets/Scripts/MenuScr/LobbyManager.cs
@@ -1,18 +1,33 @@
-﻿using System.Collections;
+﻿using Photon.Pun;
+using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
+using UnityEngine.UI;
 
-public class NewBehaviourScript : MonoBehaviour
+public class LobbyManager : MonoBehaviourPunCallbacks
 {
+    public Text LogText;
+
     // Start is called before the first frame update
     void Start()
     {
-        
+        PhotonNetwork.NickName = "Player" + Random.Range(1000, 9999);
+        Log("Player's name is get to" + PhotonNetwork.NickName);
+
+        PhotonNetwork.AutomaticallySyncScene = true;
+        PhotonNetwork.GameVersion = "1";
+        PhotonNetwork.ConnectUsingSettings();
     }
 
-    // Update is called once per frame
-    void Update()
+    public override void OnConnectedToMaster()
     {
-        
+        Log("Connected to Master");
+    }
+
+    private void Log(string message)
+    {
+        Debug.Log(message);
+        LogText.text += "\n";
+        LogText.text += message;
     }
 }
